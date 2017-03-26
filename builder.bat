1adfeac7be2fe1f67ae477a2fee52abafc937146
Andrew Reece <azmreece@gmail.com>
2017-03-26 18:01:12 +0100

    Time compilation and add default defines

--- builder.bat
+++ builder.bat
@@ -11,18 +11,26 @@ set HeaderDirectory=E:\Documents\Coding\C\h
 	for %%L in (
 		"@echo off"
 		""
-		"set CommonCompilerFlags=-nologo -MTd -fp:fast -Gm- -GR- -EHa -WX -Oi -W4 -FC -wd4201 -I%HeaderDirectory%"
+		"set CommonCompilerFlags=-nologo -MTd -fp:fast -Gm- -GR- -EHa -WX -Oi -W4 -FC -wd4201 -wd4204 -I%HeaderDirectory% -DINTERNAL=1 -DDEBUG_PREFIX=Main"
 		"set DebugCompilerFlags=-Od -Z7"
 		"set CommonLinkerFlags=-incremental:no -opt:ref"
+		"REM user32.lib gdi32.lib"
 		""
 		"IF NOT EXIST %BuildDirectory% mkdir %BuildDirectory%"
 		"pushd %BuildDirectory%"
 		""
 		"call %ShellDirectory%\shell64.bat"
 		""
-		"timethis cl %%CommonCompilerFlags%% %%DebugCompilerFlags%% %SourceDirectory%\%1\%1.c -Fm%1.map /link %%CommonLinkerFlags%% -PDB:%1_%%random%%.pdb"
-		"echo Finished at %%time%%"
+		"del *.pdb > NUL 2> NUL"
+		"echo WAITING FOR PDB > lock.tmp"
 		""
+		"timethis cl %%CommonCompilerFlags%% %%DebugCompilerFlags%% %SourceDirectory%\%1\%1.c -Fm%1.map -LD /link %%CommonLinkerFlags%% -PDB:%1_%%random%%.pdb -EXPORT:UpdateAndRender"
+		"del lock.tmp"
+		""
+		"timethis cl %%CommonCompilerFlags%% %%DebugCompilerFlags%% %SourceDirectory%\%1\win32_%1.c -Fmwin32_%1.map /link %%CommonLinkerFlags%%"
+		""
+		"echo Finished at %%time%%"
+		"REM ./win32_%1.exe"
 		"popd"
 		""
 		""
@@ -53,6 +61,13 @@ set HeaderDirectory=E:\Documents\Coding\C\h
 		"REM C4100: unreferenced formal parameter"
 		"REM C4189: local variable is initialized but not referenced"
 		"REM C4204: nonstandard extension used: non-constant aggregate initializer"
+	) do echo. %%~L
+)
+
+>taghl_config.txt (
+	for %%L in (
+		"IncludeLocals:False"
+		"UserLibraries:E:\Documents\Coding\C\windowskit_types_c.taghl,E:\Documents\Coding\C\h\types_c.taghl"
 	) do echo. %%~L
 )
 endlocal
